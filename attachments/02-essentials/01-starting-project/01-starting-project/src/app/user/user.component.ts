40e57aa6748d9c803c78ee1f6c80ae9863f867ba
rvaz55 <rvazcue@gmail.com>
2024-07-26 15:15:45 -0400

    Finished modules <24

--- attachments/02-essentials/01-starting-project/01-starting-project/src/app/user/user.component.ts
+++ attachments/02-essentials/01-starting-project/01-starting-project/src/app/user/user.component.ts
@@ -1,4 +1,9 @@
 import { Component } from '@angular/core';
+//Import the dummy-users.ts file
+import { DUMMY_USERS } from '../dummy-users';
+
+//Create function that picks a random number in index
+const randomIndex = Math.floor(Math.random() * DUMMY_USERS.length)
 
 @Component({
   selector: 'app-user',
@@ -8,5 +13,7 @@ import { Component } from '@angular/core';
   styleUrl: './user.component.css'
 })
 export class UserComponent {
+//Note: 'var' or 'const' were not needed to delcare the 'selectedUser' variable
+  selectedUser = DUMMY_USERS[randomIndex];
 
 }
